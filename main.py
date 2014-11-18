ecd3d0a7e59c72e434f54fceca447d0967110b86
Rayan <rayan.anvaraly@eleve.egd.mg>
2014-11-18 07:03:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
 # programme principal
 print ("Hello World")
+choix=input("choisissez 1 pour base10 vers base16 ou 2 pour base16 vers base10: ")
+def dec(a): 
+    if a<=9:
+        return a
+    
+    if a==10:
+        a='A'
+    elif a==11:
+        a='B'
+    elif a==12:
+        a='C'
+    elif a==13:
+        a='D'
+    elif a==14:
+        a='E'
+    elif a==15:
+        a='F'
+    return a
+
+if str(choix)=="1":
+    a=int(input("entrez un nombre en decimal(base10): "))
+    n=a #précedement "a" a été définie par int() donc on doit présenté une autre variable qui est considéré comme une valeur.
+    ch=''
+    while a<=15:
+        print(dec(a))
+        break #break permet d'interrompre une boucle.
+    while a>=16:
+        i=n%16 #i est le reste de la division euclédienne de n par 16.
+        if n<16:
+            ch=ch+str(dec(i)) #on rajoute un terme à la chaine de caractère.
+            break
+        else:
+            ch=ch+str(dec(i))
+        n=n//16 #ce calcule recommence tant que le nouveau n obtenu est superieur à 15.
+    nch=""
+    for x in reversed(ch): #pour inverser la chaine de caractère.
+        nch=nch+str(x)
+    print(nch)
+#on a utilisé str() car on ne peut pas ajouté a un str un int().
+if str(choix)=="2":
 def hexa(a):
     if str(a)<'10':
         a=str(a)
